aae8fa4a5bfe2fcd6934263ef23953bdb27bcba9
SrihanUppala <srihanuppala1408@gmail.com>
2026-08-13 12:37:40 +0530

    Fixed CORS Origin issue

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "https://askmynotes-frontend-eize.onrender.com",
+        "https://askmynotes-frontend-p06p.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
